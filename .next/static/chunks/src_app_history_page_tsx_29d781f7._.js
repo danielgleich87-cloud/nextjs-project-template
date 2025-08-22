e43e6a92cf5c1bb30b1e31aa5f8bc2fe1d90ab9c
danielgleich87-cloud <danielgleich87@gmail.com>
2025-08-22 12:34:54 +0200

    Add .next/static/chunks/src_app_history_page_tsx_29d781f7._.js 1755858893232

---
+++ .next/static/chunks/src_app_history_page_tsx_29d781f7._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_fd534e00._.js",
+  "static/chunks/node_modules_9cdb7608._.js"
+],
+    source: "dynamic"
+});
