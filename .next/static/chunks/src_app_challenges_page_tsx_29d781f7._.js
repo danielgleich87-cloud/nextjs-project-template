20684a6f47fb0c2bf7a669904870012b45977040
danielgleich87-cloud <danielgleich87@gmail.com>
2025-08-22 12:34:43 +0200

    Add .next/static/chunks/src_app_challenges_page_tsx_29d781f7._.js 1755858882733

---
+++ .next/static/chunks/src_app_challenges_page_tsx_29d781f7._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_564f4f70._.js",
+  "static/chunks/src_4e592d2f._.js"
+],
+    source: "dynamic"
+});
