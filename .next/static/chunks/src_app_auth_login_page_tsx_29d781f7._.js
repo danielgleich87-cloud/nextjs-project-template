2b12af17f2f476563cc84e49df0547dc4f91b1ac
danielgleich87-cloud <danielgleich87@gmail.com>
2025-08-22 12:34:47 +0200

    Add .next/static/chunks/src_app_auth_login_page_tsx_29d781f7._.js 1755858886597

---
+++ .next/static/chunks/src_app_auth_login_page_tsx_29d781f7._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_e90180bf._.js",
+  "static/chunks/src_bd54ac67._.js"
+],
+    source: "dynamic"
+});
